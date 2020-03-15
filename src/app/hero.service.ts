0f17c80e7aa3cbd9e9e8ab1a1701857bb56d6fd8
eyob <eyob23@gmail.com>
2020-03-15 11:38:45 -0400

    Remove file no longer needed

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,7 +4,6 @@ import { catchError, map, tap } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { MessagesService } from './messages.service';
 
 @Injectable({
